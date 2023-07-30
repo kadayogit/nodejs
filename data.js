c90a15b0b8b2c85404be3ac532a9130d3416a392
kadayogit <guledodowaa@gmail.com>
2023-07-30 15:59:28 +0300

    first upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name: "Guled Mohamed", email: "admin@gmail.com", address: 'Hargeisa, somaliland'},
+    {name: "Mohamed Bashir", email: "finance@gmail.com", address: 'Hargeisa, somaliland'},
+    {name: "Khadra Isak", email: "info@gmail.com", address: 'Hargeisa, somaliland'},
+    {name: "Kennedy Wachira", email: "ken@gmail.com", address: 'Hargeisa, somaliland'}
+]
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const data = process.argv;
+
+if(data[2]== "add"){
+    fs.writeFileSync(data[2], data[3]);
+}
+else if(data[2]=="remove"){
+    fs.unlinkSync(data[3]);
+}
+else {
+    console.warn("Invalid data input!");
+}
+
+
