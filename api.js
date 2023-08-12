11fbea82f2458eb035ebf9346beb94246b8293bd
kadayogit <guledodowaa@gmail.com>
2023-08-12 19:55:29 +0300

    Added post api to insert data into database

--- api.js
+++ api.js
@@ -2,19 +2,24 @@ const dbConnection = require('./mongodb');
 const express = require('express');
 const app = express();
 //create req and res route
+
+//middleware
+app.use(express.json());
+
+//get api 
 app.get('/', async(req, res) => {
     //get data from mongodb
     let data = await dbConnection();
     let result = await data.find({}).toArray();
-
     res.send(result);
 });
 
+//post api or insert data into the collection
 app.post('/', async(req, res) => {
     //get data from mongodb
     let data = await dbConnection();
-    let result = await data.insertOne({});
-
-    res.send(result);
+    let result = await data.insertOne(req.body);
+    res.send(req.body);
+    console.log(result);
 });
 app.listen(5000);
