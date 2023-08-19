fba52bcd726059d3986780b4a569318fc62d3136
kadayogit <guledodowaa@gmail.com>
2023-08-19 11:12:25 +0300

    connected nodejs with mongoose package and creating CRUD functionality

--- mongoose.js
+++ mongoose.js
@@ -1,17 +1,44 @@
 const mongoose = require('mongoose');
+ mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+//create schema outside of the function to access everywhere
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+});
 
-const main = async ()=> {
-    await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
-    const productSchema = new mongoose.Schema({
-        name:String,
-        price:Number
-    });
+const saveDB = async ()=> {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
     //create model
     const Product = mongoose.model('products', productSchema);
-    let data = new Product({name: "Note 7", price:200});
+    let data = new Product({name: "Note 10", price:1200, brand:"samsung", category:"mobile"});
     const result = await data.save();
     console.log(result);
 }
-
 //call the function
-main();
+saveDB();
+
+const displayDB = async () => {
+ //await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+ const Product = mongoose.model('products', productSchema);
+ let data = await Product.find({});
+ console.log(data);
+}
+displayDB();
+
+const updateDB = async () => {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.updateOne({name: "Note 7"},{$set:{name:"Note 8 pro"}});
+    console.log(data);
+   }
+   //updateDB();
+
+   const deleteDB = async () => {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({name: "Note 10"});
+    console.log(data);
+   }
+   //deleteDB();
